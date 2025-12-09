c951995884a95050b55aef1b06dbe0813f5f6510
Anas <anassaeedghani0316@gmail.com>
2025-12-09 20:18:55 +0500

    Modify

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                     <div className="inner">
                         <h1 className="text-[40px] font-bold ">We are here to <span className="text-green-500">Geekinate</span> your needs</h1><br />
                         <p className="text-[20px] text-[#A8B2C1]">Geekinate is the process of getting your technology needs fulfilled by our team of Geeks.</p><br />
-                        <button data-aos="fade-up" data-aos-delay="200" className="bg-[#6DC77A] px-5 py-2 text-[20px] rounded-md hover:bg-[#66A36F] cursor-pointer">Let's connect</button>
+                        <button data-aos="fade-up" data-aos-delay="200" className="bg-red-700 px-5 py-2 text-[20px] rounded-md hover:bg-[#66A36F] cursor-pointer">Let's connect</button>
                     </div>
                 </div>
                 <div className="boxes  h-[400px] w-[90%] sm:w-[45%] sm:h-[300px]">
